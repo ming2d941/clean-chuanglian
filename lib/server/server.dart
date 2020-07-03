790484d2ad24e0a4167679cc8801578ae0419a0a
ZhangXiaoMing <zhangxiaoming@cmcm.com>
2020-07-03 21:24:07 +0800

    [CharmingData]server修改为时刻监听请求；

--- lib/server/server.dart
+++ lib/server/server.dart
@@ -4,11 +4,14 @@ import 'dart:io';
 import 'package:flutter/services.dart';
 import 'package:http/http.dart' as http;
 import 'package:get_ip/get_ip.dart';
-import 'package:sqflite/sqflite.dart';
 
 class HttpSever {
+//  var LOCAL_PATH = 'assets/htmls';
+  var LOCAL_PATH = 'assets/console';
+
 
   HttpSever();
+
 
   bind() async {
 
@@ -17,41 +20,16 @@ class HttpSever {
     print('Listening on localhost:${InternetAddress.loopbackIPv4}');
 //    print('Listening on ip:${ipAddress}');
 
-    var server = await HttpServer.bind(
-      ipAddress,
-      3000,
-    );
+      print('----------');
+      HttpServer.bind(
+        ipAddress,
+        3002,
+      ).then((server) {
+        server.listen((HttpRequest request) {
+              response(request);
+           });
+        });
 
-    await for (HttpRequest request in server) {
-      String _content = await loadLocal(request.uri.path);
-//      body.request.response.statusCode = 200;
-//      body.request.response.headers.set("Content-Type", "text/html; charset=utf-8");
-//      body.request.response.write(_content);
-//      body.request.response.close();
-
-//      print('Listening on xxxx1:${_content}');
-//      print('Listening on xxxx1:${request.uri}');
-//      print('Listening on xxxx2:${request.requestedUri}');
-
-
-
-//      var response = await http.get('https://www.baidu.com/');
-//      //If the http request is successful the statusCode will be 200
-//      if(response.statusCode == 200){
-//        String htmlToParse = response.body;
-//        print(htmlToParse);
-//      }
-
-
-
-
-      request.response
-      ..statusCode = 200
-      ..headers.set("Content-Type", '${detectMimeType(request.uri)}; charset=utf-8')
-        ..contentLength = utf8.encode(_content).length
-        ..write(_content)
-        ..close();
-    }
   }
 
   String detectMimeType(Uri uri) {
@@ -83,36 +61,50 @@ class HttpSever {
   Future<String> loadLocal(String file) async {
     print('Listening on loadLocal:${file}');
     if (file.endsWith('/')) {
-      return await rootBundle.loadString('assets/htmls/index.html');
+      return await rootBundle.loadString(LOCAL_PATH + '/index.html');
     } else if (file.contains('.')) {
-      return await rootBundle.loadString('assets/htmls' + file);
+      return await rootBundle.loadString(LOCAL_PATH + file);
     } else {
-      var path = await getDatabasesPath();
-      print('Listening on ----->>>>:${path}');
-      var dir = new Directory(path);
-      List contents = dir.listSync(recursive: true);
-      for (var fileOrDir in contents) {
-        if (fileOrDir is File) {
-          print('Listening on ----->>>>file:${fileOrDir.path}');
-          if (fileOrDir.path.endsWith('.db')) {
-            var db = await openDatabase(fileOrDir.path);
-//            Cursor c = database.rawQuery("SELECT name FROM sqlite_master WHERE type='table' OR type='view' ORDER BY name COLLATE NOCASE", null);
-            final cursor = await db.rawQuery("SELECT name FROM sqlite_master WHERE type='table' OR type='view' ORDER BY name COLLATE NOCASE");
-            var iterator = cursor.iterator;
-            while (cursor.isNotEmpty && iterator.moveNext()) {
-              var item = iterator.current;
-              if (item != null) {
-                print('Listening on ----->>>>file:${item}');
-              }
-            }
-          }
+//      if ('test111' == file) {
+//        return '1122333';
+//      }
+//      var path = await getDatabasesPath();
+//      print('Listening on ----->>>>:${path}');
+//      var dir = new Directory(path);
+//      List contents = dir.listSync(recursive: true);
+//      for (var fileOrDir in contents) {
+//        if (fileOrDir is File) {
+//          print('Listening on ----->>>>file:${fileOrDir.path}');
+//          if (fileOrDir.path.endsWith('.db')) {
+//            var db = await openDatabase(fileOrDir.path);
+////            Cursor c = database.rawQuery("SELECT name FROM sqlite_master WHERE type='table' OR type='view' ORDER BY name COLLATE NOCASE", null);
+//            final cursor = await db.rawQuery("SELECT name FROM sqlite_master WHERE type='table' OR type='view' ORDER BY name COLLATE NOCASE");
+//            var iterator = cursor.iterator;
+//            while (cursor.isNotEmpty && iterator.moveNext()) {
+//              var item = iterator.current;
+//              if (item != null) {
+//                print('Listening on ----->>>>file:${item}');
+//              }
+//            }
+//          }
 
-        } else if (fileOrDir is Directory) {
-          print(fileOrDir.path);
-          print('Listening on ----->>>>dir:${fileOrDir.path}');
-        }
-      }
+//        } else if (fileOrDir is Directory) {
+//          print(fileOrDir.path);
+//          print('Listening on ----->>>>dir:${fileOrDir.path}');
+//        }
+//      }
     }
+  }
+
+  void response(HttpRequest request) async {
+    String _content = await loadLocal(request.uri.path);
+    print('server::::::::::::${_content}');
+    request.response
+      ..statusCode = 200
+      ..headers.set("Content-Type", '${detectMimeType(request.uri)}; charset=utf-8')
+      ..contentLength = utf8.encode(_content).length
+      ..write(_content)
+      ..close();
   }
 
 }
